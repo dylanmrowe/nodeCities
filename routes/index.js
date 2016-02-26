47fc993af0f2b39a723c368044b5261a4f055433
dylanmrowe <ashboy84@gmail.com>
2016-02-25 20:33:41 -0700

    Initial Commit
    Woo

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('weather.html', { root:  'public' });
+});
+
+router.get('/', function(req, res, next) {
+  res.sendFile('style.css', { root:  'public/stylesheets' });
+});
+
+router.get('/getCity', function(req, res, next) {
+
+	console.log(req.query);
+
+	var myRe = new RegExp("^" + req.query.q);
+        console.log(myRe);
+
+  	fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+         if(err) throw err;
+	 var cities = data.toString().split("\n");
+
+         var jsonresult = [];
+
+         for(var i = 0; i < cities.length; i++) {
+          var result = cities[i].search(myRe); 
+          if(result != -1) {
+           //console.log(cities[i]);
+           jsonresult.push({city:cities[i]});
+          } 
+         }   
+	res.status(200).json(jsonresult);
+        //console.log(jsonresult);          
+	 });
+
+});
+
+module.exports = router;
